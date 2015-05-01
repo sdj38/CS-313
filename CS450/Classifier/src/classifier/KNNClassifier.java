c864778cf78e5fc1a71b40ed58adf2ee1b4f837d
sdj38 <sjones38@live.com>
2015-04-30 21:29:11 -0600

    added a nearest neighbor algorithm

---
+++ CS450/Classifier/src/classifier/KNNClassifier.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classifier;
+
+import java.util.HashMap;
+import java.util.Map;
+import weka.classifiers.Classifier;
+import weka.core.Instance;
+import weka.core.Instances;
+
+/**
+ *
+ * @author Stephen
+ */
+public class KNNClassifier extends Classifier {
+
+    Instances inputs;
+    int neighbors = 15;
+
+    @Override
+    public void buildClassifier(Instances i) throws Exception {
+        this.inputs = i;
+    }
+
+    @Override
+    public String getRevision() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double classifyInstance(Instance instnc) throws Exception {
+        
+      // the value that will end up being returned
+        double classified = 0;
+        // an array to hold the distances to find the nearest neighbors
+        double[] distances = new double[neighbors];
+        // holds the class type of the nearest neighbors
+        double[] type = new double[neighbors];
+
+        for (int i = 0; i < inputs.numInstances(); i++) {
+            double distance = 0;
+            // sets the array to the size of the number of attributes to get the distances
+            double[] attr = new double[inputs.numAttributes()];
+            // goes through the attributes and loads the double array
+            for (int j = 0; j < inputs.numAttributes(); j++) {
+                if(inputs.instance(i).attribute(j).isNominal()){
+                    // if its a nominal value assign it the value of its index
+                   attr[j] = inputs.instance(i).attribute(j).index();
+                        
+                }else{
+                
+                attr[j] = inputs.instance(i).value(j);
+                }
+            }
+            // calculate the distance of the object to be classified and
+            // the other data points
+            for (int k = 0; k < inputs.numAttributes(); k++) {
+                distance += Math.abs(instnc.value(k) - attr[k]);
+
+            }
+            // find which distances are closest to get your neighbors
+            for (int n = 0; n < neighbors; n++) {
+                if (distance < distances[n]) {
+                    distances[n] = distance;
+                    type[n] = inputs.instance(i).classValue();
+                    break;
+                }
+                if (distances[n] == 0) {
+                    distances[n] = distance;
+                    type[n] = inputs.instance(i).classValue();
+                    break;
+                }
+
+            }
+
+        }
+       // holds the class type with the amount of times its neighbor is near by
+        HashMap<Double, Integer> results = new HashMap();
+        for (int i = 0; i < neighbors; i++) {
+            double temp = type[i];
+            if (results.containsKey(temp)) {
+                results.put(temp, results.get(temp) + 1);
+            } else {
+                results.put(temp, 1);
+            }
+        }
+        Map.Entry<Double, Integer> maxEntry = null;
+        // loops through and finds the key with the largest value
+        for (Map.Entry<Double, Integer> entry : results.entrySet()) {
+            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+                maxEntry = entry;
+            }
+        }
+
+        classified = maxEntry.getKey();
+
+        return classified;//To change body of generated methods, choose Tools | Templates.
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getNeighbors() {
+        return neighbors;
+    }
+
+    public void setNeighbors(int neighbors) {
+        this.neighbors = neighbors;
+    }
+    
+}
--- CS450/Classifier/src/classifier/Main.java
+++ CS450/Classifier/src/classifier/Main.java
@@ -6,9 +6,14 @@
 
 package classifier;
 
+import java.util.Random;
 import weka.classifiers.Evaluation;
+import weka.classifiers.lazy.IBk;
 import weka.core.Attribute;
+import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.neighboursearch.LinearNNSearch;
+import weka.filters.unsupervised.attribute.Standardize;
 import weka.filters.unsupervised.instance.RemovePercentage;
 
 /**
@@ -24,6 +29,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
+        Random rand = new Random();
         ReadFile reader = new ReadFile();
         reader.read();
         // sets data equal to what the reader class gathers
@@ -32,22 +38,66 @@ public class Main {
         HardCodeClassifier hardCode = new HardCodeClassifier();
         // randomize the data set
         data.randomize(data.getRandomNumberGenerator(data.numInstances()));
+        Standardize standard = new Standardize();
+        KNNClassifier knn = new KNNClassifier();
+       
+      //  Instance b = data.instance(rand.nextInt(150));
+      //  System.out.println(b.classValue());
+         
+       // knn.classifyInstance(b);
+      
+       
+//         Evaluation trainEval = new Evaluation(data);
+//         
+//        trainEval.evaluateModel(knn, data);
+//        System.out.println(trainEval.toSummaryString());
         
         int trainNum = (int) (data.numInstances()*.7d);
         int testNum = data.numInstances() - trainNum;
         // seperate into a training set and a test set
         Instances train = new Instances(data,0,trainNum);
         Instances test = new Instances (data,trainNum,testNum);
-        // evaluate the training set against the hardcode
-        Evaluation trainEval = new Evaluation(train);
-        trainEval.evaluateModel(hardCode, train);
-        System.out.println(trainEval.toSummaryString());
-         // evaluate the test set against the hardcode
-        Evaluation testEval = new Evaluation(test);
-        testEval.evaluateModel(hardCode, test);
-        System.out.println(testEval.toSummaryString());
         
-        // TODO code application logic here
+         knn.buildClassifier(train);
+           int correct = 0;
+        int total = 0;
+        for(int i = 0; i < test.numInstances(); i++){
+            Instance check = test.instance(i);
+          //  System.out.println(check.classValue());
+            if (check.classValue() == knn.classifyInstance(check)){
+                correct ++;
+            }
+            total ++;
+        }
+        Evaluation knnEval = new Evaluation(test);
+      //  knnEval.evaluateModel(knn,test);
+       // System.out.println(knnEval.toSummaryString());
+        System.out.println(correct + "out of" + total);
+//        // evaluate the training set against the hardcode
+//        Evaluation trainEval = new Evaluation(train);
+//        trainEval.evaluateModel(hardCode, train);
+//        System.out.println(trainEval.toSummaryString());
+//         // evaluate the test set against the hardcode
+//        Evaluation testEval = new Evaluation(test);
+//        testEval.evaluateModel(hardCode, test);
+//        System.out.println(testEval.toSummaryString());
+//        
+//        LinearNNSearch neighborSearch = new LinearNNSearch(train);
+//        Instance toIdentify;
+//        
+//        // TODO code application logic here
+//        toIdentify = new Instance(train.instance(rand.nextInt(train.numInstances())));
+//        System.out.println(toIdentify);
+//        System.out.println(neighborSearch.kNearestNeighbours(toIdentify, 3));
+//        
+//        IBk kNN = new IBk();
+//        kNN.buildClassifier(train);
+//        kNN.setKNN(15);
+//        System.out.println(kNN.getKNN());
+//        Evaluation closeEval = new Evaluation(train);
+//        closeEval.evaluateModel(kNN,train);
+//        System.out.println(closeEval.toSummaryString());
+        //d(p, q) = \sqrt{(p_1- q_1)^2 + (p_2 - q_2)^2+\cdots+(p_i - q_i)^2+\cdots+(p_n - q_n)^2}.
     }
     
    
